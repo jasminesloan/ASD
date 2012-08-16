254b01e702a82b5f0a8b5da30fdba6b2d23651e5
jasminesloan <jasminesloan@fullsail.edu>
2012-08-16 11:10:34 -0400

    won't let me switch branches so I have to commit

---
+++ Week3/Gold/xhr/data.php
@@ -0,0 +1,141 @@
+{"item":[
+			{
+			"location":["ZipCode:", "30082"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "2"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Please add more music"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Place in Que"],
+			"quantity": ["Quantity:", "7"],
+			"myDate": ["Date:", "2012-6-22"],
+			"suggestions": ["Suggestions:", "Keep up the good work!"]
+			},
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]	
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+			},
+			{
+			"location":["ZipCode:", "30106"],
+			"purchase": ["Purchase:", "Now"],
+			"quantity": ["Quantity:", "5"],
+			"myDate": ["Date:", "2012-5-22"],
+			"suggestions": ["Suggestions:", "Great music!"]
+		}
+]}
